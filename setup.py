4a261949dd854639ea14f48a31c6cab4c965cbbe
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-18 18:58:47 +0000

    Updated config files.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ sys.path.append('.')
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
 setup(
-		description='Python interface to the Octopus Energy API.',
+		description="Python interface to the Octopus Energy API.",
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
